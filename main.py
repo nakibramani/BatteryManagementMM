535eec6cf6c9573597fcfba98f4bfa324fe6ebbf
nakibramani <58126473+nakibramani@users.noreply.github.com>
2021-06-04 16:52:43 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from BatteryManagementMM import Kel103, KoradUdpComm, BMDatabase
-comm = KoradUdpComm("10.11.1.16", "10.11.0.200")
+import kel103
+from kel103 import Kel103
+from communication import KoradUdpComm
+from BMDatabase import *
+
+
+comm = KoradUdpComm("10.11.1.97", "10.11.0.200")
 kel = Kel103(comm)
 test = mainClass(kel)
 test.Home()
